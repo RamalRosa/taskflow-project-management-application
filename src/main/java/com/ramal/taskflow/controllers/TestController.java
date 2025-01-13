912708e16e7e45e3fcd1bb84dd3e2247354fa534
RamalRosa <chamalkarosa@gmail.com>
2025-01-13 22:36:55 +0530

    first commit

---
+++ src/main/java/com/ramal/taskflow/controllers/TestController.java
@@ -0,0 +1,18 @@
+package com.ramal.taskflow.controllers;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@Slf4j
+public class TestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        log.info("hello 👋");
+        return "Hello World from Test Controller 👋";
+    }
+}
